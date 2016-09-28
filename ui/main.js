972d9f094732277e0837579713f9d3f288cc1b8e
Nirmal Guna <nirmal.sheva@gmail.com>
2016-09-28 19:48:19 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,23 @@
-console.log('Loaded!');
+//Counter Code
+var countShow = document.getElementsByTagName("span")[2];
+
+function counterClick(){
+    
+    //Create a request Object    
+    var request = new XMLHttpReuest();
+    
+    //Capture the response and store it
+    request.onreadyStateChange = function() {
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //take some action
+            if(request.status === 200){
+                var counter = request.responseText;
+                countShow.innerHTML = counter;
+            }
+        }
+    }; 
+    
+    //Make the request
+    request.open('GET', "https://nirmal93guna.imad.hasura-app.io/counter", true);
+    request.send(null);
+}
